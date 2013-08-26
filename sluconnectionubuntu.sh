2dcb94f1fd86ee1e92d3bbde2a3a385a75512d29
CajuCLC <cajuclc@gmail.com>
2013-08-26 17:34:05 -0500

    Connection to Servers

---
+++ sluconnectionubuntu.sh
@@ -0,0 +1,12 @@
+echo -n "SERVER: "
+read SERVER
+echo -n "IP: "
+read IP
+echo -n "PASSWORD: "
+read PASSWORD
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no root@$IP wget https://raw.github.com/CajuCLC/slucheck/master/slucentos.sh;
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no root@$IP chmod 777 slucentos.sh;
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no root@$IP ./slucentos.sh
+scp root@$IP:/root/$SERVER.txt .
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no root@$IP rm -rf $SERVER.txt
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no root@$IP rm -rf sluubuntu.sh
