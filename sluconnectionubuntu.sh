03154c3eb677e4ea375dae71d2aa151d028f6479
CajuCLC <cajuclc@gmail.com>
2013-11-29 17:02:46 -0600

    Updated sluconnectionubuntu.sh for better usage
    Changed how the connections are made to the customer server.

--- sluconnectionubuntu.sh
+++ sluconnectionubuntu.sh
@@ -2,11 +2,17 @@ echo -n "SERVER: "
 read SERVER
 echo -n "IP: "
 read IP
-echo -n "PASSWORD: "
-read PASSWORD
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no root@$IP wget https://raw.github.com/CajuCLC/slucheck/master/slucentos.sh;
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no root@$IP chmod 777 slucentos.sh;
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no root@$IP ./slucentos.sh
-scp root@$IP:/root/$SERVER.txt .
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no root@$IP rm -rf $SERVER.txt
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no root@$IP rm -rf sluubuntu.sh
+KEYFILE=/tmp/slu
+
+rm -f $KEYFILE*
+ssh-keygen -t rsa -f $KEYFILE -P '' 1> /dev/null
+sed -i s/$(whoami)@$HOSTNAME/sluslu/ $KEYFILE.pub
+ssh-copy-id -i $KEYFILE.pub root@$IP 1> /dev/null
+ssh -o StrictHostKeyChecking=no -i $KEYFILE root@$IP wget https://raw.github.com/CajuCLC/slucheck/master/sluubuntu.sh;
+ssh -o StrictHostKeyChecking=no -i $KEYFILE root@$IP chmod 777 sluubuntu.sh;
+ssh -o StrictHostKeyChecking=no -i $KEYFILE root@$IP ./sluubuntu.sh
+scp -o StrictHostKeyChecking=no -i $KEYFILE root@$IP:/root/$SERVER.txt .
+ssh -o StrictHostKeyChecking=no -i $KEYFILE root@$IP rm -f $SERVER.txt
+ssh -o StrictHostKeyChecking=no -i $KEYFILE root@$IP rm -f sluubuntu.sh
+ssh -o StrictHostKeyChecking=no -i $KEYFILE root@$IP "sed -i /sluslu/d /root/.ssh/authorized_keys"
+rm -f $KEYFILE*
